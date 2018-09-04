464b1ec5ba016807fd2a5ba5c14c3008df3f7eb7
SirJUST <theoperaking@yahoo.com>
2018-09-04 11:03:37 -0700

    let's start learning

---
+++ anonymous_functions_lambdas/anonymous_functions_lambdas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous_functions_lambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> listEmployees = new List<Employee>()
+            {
+                new Employee{ ID = 101, Name = "Mark"},
+                new Employee{ ID = 102, Name = "John"},
+                new Employee{ ID = 103, Name = "Mary"},
+            };
+
+            // step 2
+            //Predicate<Employee> employeePredicate = new Predicate<Employee>(FindEmployee);
+
+            // we want to find the employee with id 102
+            // step 3
+            Employee employee = listEmployees.Find(delegate(Employee emp) {
+                return emp.ID == 102;
+            });
+            Console.WriteLine("ID = {0}, Name = {1}", employee.ID, employee.Name);
+            Console.Read();
+        }
+
+        // step 1
+        //public static bool FindEmployee(Employee emp)
+        //{
+        //    return emp.ID == 102;
+        //}
+
+        public class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
